ca25ccf68f3bd578a6988a5e00fb6e4a34eff5c3
shnartho <shnartho@gmail.com>
2022-03-11 10:22:40 +0100

    updated

--- List_8/Task_2/IShape.java
+++
@@ -1,6 +0,0 @@
-package Exercises.List_8.Task_2;
-
-// square, rectangle, triangle, circle
-public interface IShape {
-    double getArea();
-}
--- List_8/Task_4/FuelType.java
+++
@@ -1,7 +0,0 @@
-package Exercises.List_8.Task_4;
-
-public enum FuelType {
-    GASOLINE,
-    DIESEL,
-    ELECTRIC,
-}
